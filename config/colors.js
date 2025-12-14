38f78e3e2330fedc2fc6ba218eec6788d82fac64
TechGenius1234 <evan.degroot.network@outlook.com>
2025-12-14 14:54:26 -0800

    Add color configuration in colors.js

---
+++ config/colors.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mainBlue: "#025492",
+  accentBlue: "#3A8CD1",
+  success: "#00CC00",
+  error: "#C23535",
+  warning: "#E6A627",
+  greyDark: "#2E2E2E",
+  greyLight: "#D9D9D9"
+};
